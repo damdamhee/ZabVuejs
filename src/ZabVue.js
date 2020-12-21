5395b4df5c05542267160f9df357a053e6752355
JinHyung Jung <jh9310s@gmail.com>
2020-12-22 01:42:33 +0900

    [수정] ZabVue.js
    - ZabVue 객체에 name 속성 추가 (name : 컴포넌트 명)
    - 자식 컴포넌트 ZabVue 객체를 받을 수 있도록 수정
    - render() 호출 시점을 mount 시점으로 변경

--- src/ZabVue.js
+++ src/ZabVue.js
@@ -4,6 +4,8 @@ import render from "./render.js";
 
 export default class ZabVue {
   constructor(attributes) {
+    this.name = attributes.name;
+
     //set lifecycle methods;
     this.onCreated = attributes.onCreated || (() => {});
     this.onMounted = attributes.onMounted || (() => {});
@@ -12,17 +14,25 @@ export default class ZabVue {
     //apply reactive properties
     this.props = attributes.props;
     this.components = attributes.components; //자식 VDOM 노드
-    this.data = createData.call(this, attributes.data);
+    this.data = createData.call(
+      this,
+      attributes.data ||
+        (() => {
+          return {};
+        })
+    );
     this.methods = attributes.methods;
 
     let createElementFunc = attributes.render;
 
     this.render = () => {
-      let elem = createElementFunc.call(this, createElement);
-      this.rootNode = render.call(this, elem);
+      this.rootElement = createElementFunc.call(this, createElement);
+      this.rootNode = render.call(this, this.rootElement);
       return this.rootNode;
     };
-    this.rootNode = this.render();
+
+    //ZabVue 객체 생성 시에는 단순히 VDOM 객체만 생성한 상태이다
+    this.rootElement = createElementFunc.call(this, createElement);
 
     this.onCreated();
     //dependencyTable을 생성한 후, 만약 데이터에 변경이 발생한다면, 화면을 갱신해야 한다 (다시 마운트 호출)
@@ -31,9 +41,8 @@ export default class ZabVue {
     //targetElementId에 해당하는 DOM에 마운트
     let targetElementNode = document.getElementById(targetElementId);
     try {
-      if (targetElementNode.hasChildNodes())
-        targetElementNode.removeChild(targetElementNode.firstChild);
-      targetElementNode.appendChild(this.rootNode);
+      this.render();
+      targetElementNode.replaceWith(this.rootNode);
     } catch (error) {
       console.log(`Cannot Find DOM with ID ${targetElementNode}`);
       return;
