4b9bdd09bd6a089dba81dab2ed92339476f91868
JinHyung Jung <jh9310s@gmail.com>
2020-12-22 00:30:25 +0900

    [수정] ZabVue.js
    - render 함수 수정(render가 호출되면 루트 노드도 재생성하도록 변경)

--- src/ZabVue.js
+++ src/ZabVue.js
@@ -1,4 +1,5 @@
 import createElement from "./createElement.js";
+import createData from "./createData.js";
 import render from "./render.js";
 
 export default class ZabVue {
@@ -6,15 +7,17 @@ export default class ZabVue {
     //apply reactive properties
     this.props = attributes.props;
     this.components = attributes.components; //자식 VDOM 노드
-    this.data = attributes.data();
+    this.data = createData.call(this, attributes.data);
     this.methods = attributes.methods;
 
     let createElementFunc = attributes.render;
-    let rootElement = createElementFunc(createElement);
-    this.render = render;
-    
-    this.rootNode = render(rootElement);
 
+    this.render = () => {
+      let elem = createElementFunc.call(this, createElement);
+      this.rootNode = render.call(this, elem);
+      return this.rootNode;
+    };
+    this.rootNode = this.render();
 
     //dependencyTable을 생성한 후, 만약 데이터에 변경이 발생한다면, 화면을 갱신해야 한다 (다시 마운트 호출)
   }
@@ -22,10 +25,12 @@ export default class ZabVue {
     //targetElementId에 해당하는 DOM에 마운트
     let targetElementNode = document.getElementById(targetElementId);
     try {
-        targetElementNode.replaceWith(this.rootNode);
-    } catch(error) {
-        console.log(`Cannot Find DOM with ID ${targetElementNode}`)
-        return;
+      if (targetElementNode.hasChildNodes())
+        targetElementNode.removeChild(targetElementNode.firstChild);
+      targetElementNode.appendChild(this.rootNode);
+    } catch (error) {
+      console.log(`Cannot Find DOM with ID ${targetElementNode}`);
+      return;
     }
   }
 }
