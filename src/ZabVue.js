35222b35fbaf016777e67c90be87ff35d7bf0a99
JinHyung Jung <jh9310s@gmail.com>
2020-12-22 00:37:48 +0900

    [수정] ZabVue - Lifecycle 이벤트 핸들러 추가
    - onCreated, onMounted, onUpdated 핸들러 호출 기능 추가

--- src/ZabVue.js
+++ src/ZabVue.js
@@ -4,6 +4,11 @@ import render from "./render.js";
 
 export default class ZabVue {
   constructor(attributes) {
+    //set lifecycle methods;
+    this.onCreated = attributes.onCreated || (() => {});
+    this.onMounted = attributes.onMounted || (() => {});
+    this.onUpdated = attributes.onUpdated || (() => {});
+
     //apply reactive properties
     this.props = attributes.props;
     this.components = attributes.components; //자식 VDOM 노드
@@ -19,6 +24,7 @@ export default class ZabVue {
     };
     this.rootNode = this.render();
 
+    this.onCreated();
     //dependencyTable을 생성한 후, 만약 데이터에 변경이 발생한다면, 화면을 갱신해야 한다 (다시 마운트 호출)
   }
   mount(targetElementId) {
@@ -32,5 +38,7 @@ export default class ZabVue {
       console.log(`Cannot Find DOM with ID ${targetElementNode}`);
       return;
     }
+
+    this.onMounted();
   }
 }
